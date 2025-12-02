d2d7e7f50b683c7eb15cb59e759c97da895efcf8
Yerlin A. Rodriguez <yerlin.alec21@gmail.com>
2025-12-02 17:38:48 -0400

    Home page creation

--- server.js
+++ server.js
@@ -5,37 +5,48 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger.json');
-
+const path = require('path');
 const themeRoutes = require('./routes/themeRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// DB
+// Connect DB
 connectDB();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Root
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Portfolio API is running' });
-});
+// ----------------------------------------
+// SERVE PUBLIC HOMEPAGE FIRST
+// ----------------------------------------
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// ----------------------------------------
+// API ROUTES
+// ----------------------------------------
 app.use('/api/themes', themeRoutes);
 app.use('/api/users', userRoutes);
 
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// 404
+// ----------------------------------------
+// Health check (optional)
+// ----------------------------------------
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Portfolio API is running' });
+});
+
+// ----------------------------------------
+// 404 Handler
+// ----------------------------------------
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-// Error handler
+// Global Error Handler
 app.use((err, req, res, next) => {
   console.error('Global error:', err);
   res.status(500).json({ message: 'Internal server error', error: err.message });
